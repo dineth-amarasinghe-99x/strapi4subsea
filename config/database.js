c03774d9c86797cd628ebffe0d97ecf4922e8270
Dineth Amarasinghe <dinetha@99x.io>
2021-10-13 11:49:11 +0530

    enable cloud

--- config/database.js
+++ config/database.js
@@ -1,37 +1,15 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'mongoose',
-//       settings: {
-//         host: env('DATABASE_HOST'),
-//         srv: env.bool('DATABASE_SRV', true),
-//         port: env.int('DATABASE_PORT', 27017),
-//         database: env('DATABASE_NAME'),
-//         username: env('DATABASE_USERNAME'),
-//         password: env('DATABASE_PASSWORD'),
-//       },
-//       options: {
-//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-//         ssl: env.bool('DATABASE_SSL', true),
-//       },
-//     },
-//   },
-// });
-
-
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.vlbxi.mongodb.net'),
+        host: env('DATABASE_HOST'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'STRAPI4SUBSEA'),
-        username: env('DATABASE_USERNAME', 'Admin'),
-        password: env('DATABASE_PASSWORD', '1qaz2wsx'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
@@ -40,3 +18,25 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         host: env('DATABASE_HOST', 'cluster0.vlbxi.mongodb.net'),
+//         srv: env.bool('DATABASE_SRV', true),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME', 'STRAPI4SUBSEA'),
+//         username: env('DATABASE_USERNAME', 'Admin'),
+//         password: env('DATABASE_PASSWORD', '1qaz2wsx'),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', true),
+//       },
+//     },
+//   },
+// });
