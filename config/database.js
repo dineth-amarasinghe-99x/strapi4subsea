0567905fa87e72b72c2d94d13f5598b92db6e6e7
Dineth Amarasinghe <dinetha@99x.io>
2021-10-22 03:02:50 +0530

    azure deployment

--- config/database.js
+++ config/database.js
@@ -1,19 +1,40 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         host: env('DATABASE_HOST'),
+//         srv: env.bool('DATABASE_SRV', true),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME'),
+//         username: env('DATABASE_USERNAME'),
+//         password: env('DATABASE_PASSWORD'),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', true),
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: 'bookshelf',
       settings: {
-        host: env('DATABASE_HOST'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        client: "mysql",
+        host: env('DATABASE_HOST', 'strapi-cms-database.mysql.database.azure.com'),
+        port: env('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi-cms@strapi-cms-database'),
+        password: env('DATABASE_PASSWORD', 'Sea@1234'),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        useNullAsDefault: true,
+        ssl: false
       },
     },
   },
--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,28 @@
+// module.exports = ({ env }) => ({
+//     upload: {
+//       provider: 'cloudinary',
+//       providerOptions: {
+//         cloud_name: env('CLOUDINARY_NAME'),
+//         api_key: env('CLOUDINARY_KEY'),
+//         api_secret: env('CLOUDINARY_SECRET'),
+//       },
+//       actionOptions: {
+//         upload: {},
+//         delete: {},
+//       },
+//     },
+//   });
+
 module.exports = ({ env }) => ({
-    upload: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
-    },
-  });
+  upload: {
+    provider: 'azure-storage',
+    providerOptions: {
+      account: env('STORAGE_ACCOUNT','strapicmsstorage'),
+      accountKey: env('STORAGE_ACCOUNT_KEY', 'PfX1jY5fP1ADcT2TH/e/ujvZ1VrPghJ6iWXVSbpE9tt5ou5CtMK5scb0fdq4PKtRKJzBCULG1NsNCm6svsUqoA=='),
+      containerName: env('STORAGE_CONTAINER_NAME', 'strapi-uploads'),
+      cdnBaseURL: env('STORAGE_CDN_URL', 'https://strapicmsstorage.blob.core.windows.net'),
+      defaultPath: 'assets',
+      maxConcurrent: 10
+    }
+  }
+});
